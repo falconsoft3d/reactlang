25c7c7a8a1589483852bb91b6652a64a182e5eee
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-10-03 16:00:15 +0200

    sc

--- src/components/home/WhiteBox/WhiteBox.js
+++ src/components/home/WhiteBox/WhiteBox.js
@@ -7,8 +7,13 @@ import "./WhiteBox.css";
 
 export default function WhiteBox() {
     return (
+        <>
         <div className="white-box">
             <Link className="green" to="/game/1000">English 1000 Words</Link>
         </div>
+        <div>
+            <img  src="/qr.png" />
+        </div>
+        </> 
     )
 }
